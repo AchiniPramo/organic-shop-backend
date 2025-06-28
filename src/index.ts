0bbb21d749853bf5efdb70ae29bf317d2bece447
AchiniPramo <achinipramodhya4@gmail.com>
2025-06-28 15:19:43 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express ,{Express , Request, Response} from 'express';
+
+
+// 1. Initialize the Express App
+const app = express();
+
+// 2. Define the application port
+const port = 3000;
+
+// 3. Define a simple HTTP request
+app.get('/', (req, res) => {
+        res.send('Hello!');
+});
+
+// 4. Instruct the express app to listen on port 3000
+app.listen(port, () => {
+        console.log(`Server is running at http://localhost:${port}`);
+});
