ac7ae09d856466a3151b3b5dff38ac2d0f56636a
AchiniPramo <achinipramodhya4@gmail.com>
2025-07-05 09:16:18 +0530

    create app.ts

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,7 @@
-import express ,{Express , Request, Response} from 'express';
-
-
-// 1. Initialize the Express App
-const app = express();
+import app from "./app";
 
 // 2. Define the application port
 const port = 3000;
-
-// 3. Define a simple HTTP request
-app.get('/', (req, res) => {
-        res.send('Hello Express!');
-});
 
 // 4. Instruct the express app to listen on port 3000
 app.listen(port, () => {
