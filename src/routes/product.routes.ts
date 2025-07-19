731c036d99e917ca4b8dec1c0375295bedd3a481
AchiniPramo <achinipramodhya4@gmail.com>
2025-07-19 10:19:37 +0530

    done middleware

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,13 +1,14 @@
 import {Router} from "express";
 import {deleteProduct, getAllProducts, getProduct, saveProduct, updateProduct} from "../controllers/product.controller";
+import {authorizeRoles} from "../middleware/auth.middleware";
 
 const productRouter: Router = Router();
 
 //Handle requests
 productRouter.get("/all", getAllProducts); //Get all
-productRouter.post("/save", saveProduct ); //Save
+productRouter.post("/save", authorizeRoles('admin'), saveProduct); //Save
 productRouter.get("/:id", getProduct); //Get by id
-productRouter.put("/update/:id", updateProduct); //Update
-productRouter.delete("/delete/:id", deleteProduct); //Delete
+productRouter.put("/update/:id", authorizeRoles('admin'), updateProduct); //Update
+productRouter.delete("/delete/:id", authorizeRoles('admin'), deleteProduct); //Delete
 
 export default productRouter;
